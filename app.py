3acee8459c838224ed97ab88220e3d41404523c6
Todd Brannon <toddqbrannon@gmail.com>
2019-12-09 06:50:32 -0600

    add the nature of visit field to db

--- app.py
+++ app.py
@@ -22,13 +22,15 @@ class Feedback(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     customer = db.Column(db.String(200), unique=True)
     dealer = db.Column(db.String(200))
+    visit = db.Column(db.String(200))
     rating = db.Column(db.Integer)
     likelihood = db.Column(db.Integer)
     comments = db.Column(db.Text())
 
-    def __init__(self, customer, dealer, rating, likelihood, comments):
+    def __init__(self, customer, dealer, visit, rating, likelihood, comments):
         self.customer = customer
         self.dealer = dealer
+        self.visit = visit
         self.rating = rating
         self.likelihood = likelihood
         self.comments = comments
@@ -42,6 +44,7 @@ def submit():
     if request.method == 'POST':
         customer = request.form['customer']
         dealer = request.form['dealer']
+        visit =  request.form['visit']
         rating = request.form['rating']
         likelihood = request.form['likelihood']
         comments = request.form['comments']
@@ -49,10 +52,10 @@ def submit():
         if customer == '' or dealer == '':
             return render_template('index.html', message='Please complete all fields')
         if db.session.query(Feedback).filter(Feedback.customer == customer).count() == 0:
-            data = Feedback(customer, dealer, rating, likelihood, comments)
+            data = Feedback(customer, dealer, visit, rating, likelihood, comments)
             db.session.add(data)
             db.session.commit()
-            send_mail(customer, dealer, rating, likelihood, comments)
+            send_mail(customer, dealer, visit, rating, likelihood, comments)
             return render_template('success.html')
         return render_template('index.html', message='You have already submitted feedback')
 
